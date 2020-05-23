76aa6ee8722d658aa30f931c2cee7781101a63f3
dobrawarog <dobrawa.rog@lo1.sandomierz.pl>
2020-05-23 16:21:15 +0200

    todo

---
+++ todo/todo.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  todo.py
+#  
+#  Copyright 2020 pc <pc@DESKTOP-FN8QH4V>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+	
+	
+
+from datetime import datetime
+from flask import flash, redirect, url_for, request
+from flask import Flask, g
+from flask import render_template
+import os
+import sqlite3
+
+
+app = Flask(__name__)
+
+app.config.update(dict(
+    SECRET_KEY='bardzosekretnawartosc',
+    DATABASE=os.path.join(app.root_path, 'db.sqlite'),
+    SITE_NAME='Moje zadania'
+))
+
+
+def get_db():
+    """Funkcja tworząca połączenie z bazą danych"""
+    if not g.get('db'):  # jeżeli brak połączenia, to je tworzymy
+        con = sqlite3.connect(app.config['DATABASE'])
+        con.row_factory = sqlite3.Row
+        g.db = con  # zapisujemy połączenie w kontekście aplikacji
+    return g.db  # zwracamy połączenie z bazą
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Zamykanie połączenia z bazą"""
+    if g.get('db'):
+        g.db.close()
+
+
+@app.route('/')
+def index():
+    # return 'Cześć, tu Python!'
+    return render_template('index.html')
+
+@app.route('/zadania', methods=['GET', 'POST'])
+def zadania():
+    error = None
+    if request.method == 'POST':
+        zadanie = request.form['zadanie'].strip()
+        if len(zadanie) > 0:
+            zrobione = '0'
+            data_pub = datetime.now()
+            db = get_db()
+            db.execute('INSERT INTO zadania VALUES (?, ?, ?, ?);',
+                       [None, zadanie, zrobione, data_pub])
+            db.commit()
+            flash('Dodano nowe zadanie.')
+            return redirect(url_for('zadania'))
+
+        error = 'Nie możesz dodać pustego zadania!'  # komunikat o błędzie
+
+    db = get_db()
+    kursor = db.execute('SELECT * FROM zadania ORDER BY data_pub DESC;')
+    zadania = kursor.fetchall()
+    return render_template('zadania_lista.html', zadania=zadania, error=error)
+
+if __name__ == '__main__':
+    app.run(debug=True)
